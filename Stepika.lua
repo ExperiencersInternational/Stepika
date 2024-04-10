6f470ebd21633caca2739c582efef04029051e68
ExperiencersInternational <gamersinternationalhelp+github@gmail.com>
2024-04-10 20:27:01 +0100

    v0.2.0, widget creation move

--- Stepika.lua
+++ Stepika.lua
@@ -4,11 +4,11 @@ local Stepika = {}
 
 Stepika.PluginData = {
 	Name = "DisableCoreGUIFunctions",
-	PluginDescription = "A legacy plugin to toggle GUIs.",
-	Developer = "ExperiencersInternational",
-	Ver = "v2.2.0",
-	StepikaCore = "v0.1.2",
-	PluginLogo = ""
+	PluginDescription = "A legacy plugin to toggle GUIs.", -- A short description about your plugin
+	Developer = "ExperiencersInternational", -- Who made the plugin
+	Ver = "v2.2.0", -- The current version of your plugin
+	PluginLogo = "",
+	StepikaCore = "v0.2.1" -- Don't modify this, version of the Stepika module
 }
 
 -- Ignore all the below, this is what makes the module work!
@@ -17,44 +17,27 @@ Stepika.StudioInfo = {
 	Theme = settings().Studio.Theme.Name,
 }
 
-function Stepika.CreateWidget(PluginDirectory, WidgetMinWidth, WidgetMinHeight)
-
-	local Toolbar = plugin:CreateToolbar(Stepika.PluginData.Name .. " plugin")
-	local Launcher = Toolbar:CreateButton("Open widget", "Opens the widget for this plugin.", Stepika.PluginData.PluginLogo)
-	
-	local Widget = plugin:CreateDockWidgetPluginGui(
-		Stepika.PluginData.Name,
-		DockWidgetPluginGuiInfo.new(
-			Enum.InitialDockState.Float,
-			false,
-			false,
-			280,
-			540,
-			WidgetMinWidth,
-			WidgetMinHeight
-		)
-	)
-	
-	Widget.Title = Stepika.PluginData.Name
-	Widget.Name = Stepika.PluginData.Developer .. "." .. Stepika.PluginData.Name
-	
-	local MaxSize = 12
-	local MinSize = 6
-	local ThicknessSize = 0
-	local IsBorder = false
-	local AutoAdjustZIndex = true
-	local GlobalZindex = true
-	
-	local WidgetUI = Instance.new("Frame", Widget)
+function Stepika.AuroraCreateInitialScroll()
+	local WidgetUI = Instance.new("Frame", script.Parent)
 	WidgetUI.Name = "UI"
+	WidgetUI.BackgroundTransparency = 1
+	WidgetUI.Size = UDim2.new(1, 0, 1, 0)
+	
+	local WidgetScroll = Instance.new("ScrollingFrame", WidgetUI)
+	WidgetScroll.AutomaticCanvasSize = Enum.AutomaticSize.Y
+	WidgetScroll.BorderSizePixel = 0
+	WidgetScroll.CanvasSize = UDim2.new(0, 0, 0, 0)
+	WidgetScroll.ScrollBarThickness = 4
+	WidgetScroll.ScrollingDirection = Enum.ScrollingDirection.Y
+	WidgetScroll.Size = UDim2.new(1, 0, 1, 0)
 	
 	if Stepika.StudioInfo.Theme == "Dark" then
-		WidgetUI.BackgroundColor3 = Color3.fromRGB(46, 46, 46)
+		WidgetScroll.BackgroundColor3 = Color3.fromRGB(46, 46, 46)
 	else
-		WidgetUI.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		WidgetScroll.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 	end
 	
-	return Widget
+	return WidgetScroll
 end
 
 return Stepika
